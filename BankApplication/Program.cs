64e9c27279e3104dc3e6b61302c5d7917784eb15
Jayson Taylor <1932645@champlaincollege.qc.ca>
2020-10-14 20:36:53 -0400

    Created the one accout of each type with a starting balance of $5.00

--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -16,7 +16,9 @@ namespace BankApplication
             String Option2;
             String Option3;
 
-            Account savingsAccount = new Account();
+            Account savingsAccount = new SavingsAccount(5.00, .10);
+            Account chequingAccount = new ChequingAccount(5.00, .15);
+            Account GlobalSavingsAccount = new GlobalSavingsAccount(5.00, .15);
 
             try
             {
